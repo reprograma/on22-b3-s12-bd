99eb880cb0b92766daafbbc70f5d54a4e60e890a
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-06-12 22:06:33 -0300

    done

--- exercicios/r-library/src/controllers/booksController.js
+++ exercicios/r-library/src/controllers/booksController.js
@@ -1,97 +1,66 @@
-const BooksModel = require("../models/booksModel");
+const BooksModel = require('../models/booksModel');
 
-const getAllBooks = async (req, res) => {
+const addNewBook = async (req, res) => {
   try {
-    const allBooks = await BooksModel.find({}, null);
-    res.status(200).json(allBooks);
-  } catch {
-    res.status(500).json({ message: error.message });
-  };
-};
-
-const getBookById = async (req, res) => {
-  try {   
-    const findBook = await BooksModel.findById(req.params.id);
-    res.status(200).json(findBook);
+    const newBookData = req.body;
+    const newBook = new BooksModel(newBookData);
+    const savedBook = await newBook.save();
+    res
+      .status(201)
+      .json({ message: 'New book added successfully!', savedBook });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-const addNewBook = async (req, res) => {
+const getAllBooks = async (req, res) => {
   try {
-    const {
-      title,
-      launchYear,
-      available,
-      publisher,
-      gender,
-      writer,
-      pages
-    } = req.body;
-
-    const newBook = new BooksModel({
-      title,
-      launchYear,
-      available,
-      publisher,
-      gender,
-      writer,
-      pages
-    });
-    const savedBook = await newBook.save();
-    res
-      .status(200)
-      .json({ message: "New Book added successfully!", savedBook });
+    const allBooks = await BooksModel.find();
+    res.status(200).json(allBooks);
   } catch (error) {
-    console.error(error);
     res.status(500).json({ message: error.message });
-  };
+  }
+};
+
+const getBookById = async (req, res) => {
+  try {
+    const book = await BooksModel.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const updateBookById = async (req, res) => {
   try {
-    const {
-      title,
-      launchYear,
-      available,
-      publisher,
-      gender,
-      writer,
-      pages,
-    } = req.body;
-    const updateBook = await BooksModel.findByIdAndUpdate(
+    const updatedBookData = req.body;
+    const updatedBook = await BooksModel.findByIdAndUpdate(
       req.params.id,
-      {
-        title,
-        launchYear,
-        available,
-        publisher,
-        gender,
-        writer,
-        pages,
-      }
+      updatedBookData,
+      { new: true }
     );
-    res.status(200).json(updateBook);
-  } catch {
-  console.error(error);
-  res.status(500).json({ message: error.message });
-}
+    if (!updatedBook) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const deleteBook = async (req, res) => {
   try {
-    const { id } = req.params;
-    const findBooks = await BooksModel.findById(id);
-
-    if (findBooks == null) {
-      return res.status(404).json({ message: `Book with id ${id} not found` })
-    };
-    await findBooks.remove();
-    res.status(200).json({ message: `Book with id ${id} was successfully deleted` });
+    const book = await BooksModel.findByIdAndDelete(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.status(200).json({ message: 'Book deleted successfully!' });
   } catch (error) {
     res.status(500).json({ message: error.message });
-  };
+  }
 };
 
 module.exports = {
