16c8caf267b4116ced4a2d970e6f8224fa4b02d0
CybelleCouto <cybelinhacouto@hotmail.com>
2023-06-14 21:01:31 -0300

    atividade semana 12

---
+++ exercicios/r-library/src/models/booksModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const BookSchema = new mongoose.Schema(
+    {
+       _id:{
+        type:mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId()
+       },
+       title:{
+        type:String,
+        required:true,
+        unique:true
+       },
+       launchYear:{
+        type:Number,
+        required:true
+       },
+       available:{
+        type:Boolean,
+        required:true
+       },
+       publisher:{
+        type:String,
+        required:true
+       },
+       gender:{
+        type:String,
+        required:true
+       },
+       writer:{
+        type:String,
+        required:true
+       },
+       pages:Number
+    }
+)
+
+const Model = mongoose.model("Book",BookSchema)
+
+module.exports = Model
